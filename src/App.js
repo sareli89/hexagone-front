4c155be6e19a815cc093c6b5e5455c92064896b0
Sara Reveles <elizabethrc@gmail.com>
2022-08-31 02:35:54 -0400

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid mt-2"> 
+      <div className="row">
+        <div className="col-3">
+          <div className="bg-warning  h-100">
+            nsdjnjd
+          </div>
+        </div>
+
+        <div className="col-9">
+          <div className="bg-secondary  h-auto-">
+            kndknfdn
+          </div>
+            
+          <div className="bg-danger  h-auto">
+            ;klmdkdsk;
+          </div>
+
+          <div className="bg-info  h-auto">
+            kldkd
+          </div>
+
+        </div>
+      </div> 
       
+    
     </div>
   );
 }
